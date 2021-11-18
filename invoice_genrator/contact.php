b1812ddd7d9f108948e28c7f28558df4f8379087
Varuneshag <64423282+Varuneshag@users.noreply.github.com>
2021-11-18 18:00:34 +0530

    Add files via upload

---
+++ invoice_genrator/contact.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'includes/header.php'; ?>
+<!DOCTYPE html>
+<style type="text/css">
+body{
+background-image: url('images/bg.jpg'); background-repeat: no-repeat; background-size: cover;
+}
+</style>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<div style="background-color: #F0F0F0; margin-left: 40px; margin-right: 40px;">
+		<div class="row" style="margin-top: 15px; padding-top: 30px; padding-bottom: 30px; padding: 20px;">
+			<div class="col-sm-6 align-items-center" style="align-items: center; padding-left: 15px; padding-right: 15px;">
+				<h2 align="center">Let's Start a Conversation</h2>
+				<p align="center" style="padding: 5px;">We'd love to hear from you. Here's how you can reach us...</p>
+				<div class="box box-center" style="border-radius: 6px; background-color: orange; color: white; padding-top: 50px; padding-bottom: 50px; margin-left: 20px; margin-right: 60px;">
+					<i class="material-icons" style="font-size:36px; margin-left: 40px; padding: 20px;">mail_outline</i>
+					<div class="text" style="margin-left: 70px; color: white; font-weight: bold; font-size: 18px;"> Email
+						<br> <a href="contact@primathink.com" type="Email" style="color: white;">Varuneshagr@gmail.com</a>
+					</div>
+					<i class="material-icons" style="font-size:36px; margin-left: 40px; padding: 20px;" >call</i>
+					<div class="text" style="margin-left: 70px; color: white; font-weight: bold; font-size: 18px;">
+						<p style="color: white; font-weight: bold;">Phone <br><a href="tel:09890544466"></a> 9370xxxxx13 </p>
+					</div>
+					<i class="material-icons" style="font-size:36px; margin-left: 40px; padding: 20px;">location_on</i>
+					<div class="text" style="margin-left: 70px; color: white; font-weight: bold; font-size: 18px;">
+						<p style="color: white; font-weight: bold;">Address <br><br> Ramnagar Bazaar Chowk <br> Gondia (441614) </p>
+					</div>
+					</div>
+			</div>
+			<div class="col-sm-6 align-items-center" style="border-radius: 10px; background-color: white; margin-top: 7px; padding-left: 15px; padding-right: 15px; align-items: center;">
+				<form style="padding: 30px;">
+					<input type="text" name="Name" placeholder="NAME" class="form-control" style="padding: 20px; margin-top: 60px;">
+					<input type="Email" name="Email" placeholder="EMAIL" class="form-control" style="padding: 20px; margin-top: 60px;">
+					<input type="number" name="Phone" placeholder="PHONE NUMBER" class="form-control" style="padding: 20px; margin-top: 60px;">
+					<input type="text" name="message" placeholder="MESSAGE" class="form-control" style="padding: 20px; margin-top: 60px; padding-bottom: 120px;">
+					<button style="background-color: darkgrey; color: white; padding: 10px; padding-left: 20px; padding-right: 20px; border-radius: 6px; float: right;
+					margin-top: 30px; margin-bottom: 20px;">SEND</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="row" style="padding-top: 20px; padding-bottom: 20px;">
+		<div class="col-sm-6" style="padding-left: 120px;">
+			<h1 style="font-weight: bold; padding-bottom: 15px;">Contact Our World Class <br>Support Team</h1>
+			<p>Having queries or any questions. Feel free to ask and let your all queries be cleared with our support team.
+				<br>Give us a call and get all your confusion to be cleared.
+So what are you thinking about now?</p>
+		</div>
+		<div class="col-sm-6" style="padding-left: 90px; padding-top: 40px;">
+			<p style="font-size: 20px; font-weight: bold;"><a href="mailto: contact@primathink.com" style="color: blue">Varuneshagr@gmail.com</a></p>
+			<h2 style="padding-top: 30px; font-weight: bold;">Call: 9370xxxxx13</h2>	
+		</div>
+	</div>
+	
